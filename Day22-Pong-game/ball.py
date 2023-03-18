94e7ee2ca6f8814bfc80a925cb135ec9257a2d1a
shipowner99 <daban2@naver.com>
2023-03-18 23:28:11 +0900

    Fixing bug

--- Day22-Pong-game/ball.py
+++ Day22-Pong-game/ball.py
@@ -10,7 +10,7 @@ class Ball(Turtle):
         self.color("white")
         self.x_move = 10
         self.y_move = 10
-        self.move_speed = 0.1
+        self.move_speed = 0.08
 
     def move(self):
         new_x = self.xcor() + self.x_move
--- Day22-Pong-game/main.py
+++ Day22-Pong-game/main.py
@@ -31,9 +31,17 @@ while game_is_on:
     # 위아래 벽에 부딪히면
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
+
     # 패들에 부딪히면
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+    # (기존 강의 코드)
+    # if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+    #     ball.bounce_x()
+    # (버그 고친 코드)
+    if -370 < ball.xcor() < -330 and l_paddle.ycor() + 70 > ball.ycor() > l_paddle.ycor() - 70 or 370 > ball.xcor() > 330 and r_paddle.ycor() + 70 > ball.ycor() >r_paddle.ycor() - 70:
+        ball.bounce_y()
+    if ball.xcor() == -330 and l_paddle.ycor() + 70 > ball.ycor() > l_paddle.ycor() - 70 or ball.xcor()==330 and r_paddle.ycor() + 70 > ball.ycor() >r_paddle.ycor() - 70:
         ball.bounce_x()
+
     # 공이 끝으로 빠지면
     if ball.xcor() > 380:
         ball.reset_position()
