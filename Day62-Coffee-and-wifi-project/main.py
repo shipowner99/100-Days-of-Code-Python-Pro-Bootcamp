49bab8e144179ece5b73e011ab563848b61beaf7
shipowner99 <daban2@naver.com>
2023-03-21 16:04:24 +0900

    Making Table using cafe-data.csv at cafes.html.

--- Day62-Coffee-and-wifi-project/main.py
+++ Day62-Coffee-and-wifi-project/main.py
@@ -42,13 +42,12 @@ def add_cafe():
 
 @app.route('/cafes')
 def cafes():
-    with open('cafe-data.csv', newline='') as csv_file:
+    with open('cafe-data.csv', 'rt', encoding='UTF8', newline='') as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
         for row in csv_data:
             list_of_rows.append(row)
     return render_template('cafes.html', cafes=list_of_rows)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
