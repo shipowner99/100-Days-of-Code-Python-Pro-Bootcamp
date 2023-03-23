d84a3f2e1ad6ce9d58abf89123238dc79a1ad1b1
shipowner99 <daban2@naver.com>
2023-03-23 18:48:01 +0900

    Make Update button and Delete button work

--- Day64-Movie-project/main.py
+++ Day64-Movie-project/main.py
@@ -7,13 +7,82 @@ from wtforms.validators import DataRequired
 import requests
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
+
+db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movie-best10.db"
+db.init_app(app)
+
+app.config['SECRET_KEY'] = 'abx'
 Bootstrap(app)
+
+class MovieRatingForm(FlaskForm):
+    rating = StringField('Your Rating Out of 10 e.g. 7.5', validators=[DataRequired()])
+    review = StringField('Your Review', validators=[DataRequired()])
+    submit = SubmitField("Done")
+
+class MovieAddingForm(FlaskForm):
+    title = StringField('Movie Title', validators=[DataRequired()])
+    submit = SubmitField("Add movie")
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    title = db.Column(db.String(250), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.Text, nullable=False)
+
+with app.app_context():
+    db.create_all()
+    # new_movie = Movie(
+    #     title="Phone Booth",
+    #     year=2002,
+    #     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    #     rating=7.3,
+    #     ranking=10,
+    #     review="My favourite character was the caller.",
+    #     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+    # )
+    # db.session.add(new_movie)
+    # db.session.commit()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    all_movies = db.session.query(Movie).all()
+    return render_template("index.html", movies=all_movies)
+
+@app.route("/edit", methods=["POST", "GET"])
+def edit():
+    form = MovieRatingForm()
+    movie_id = requests.args.get('id')
+    movie_seleted = Movie.query.get(movie_id)
+    if form.validate_on_submit():
+        movie_to_update = Movie.query.get(movie_id)
+        movie_to_update.rating = float(form.rating.data)
+        movie_to_update.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('edit.html', movie = movie_seleted, form=form)
+
+@app.route('/delete')
+def delete():
+    movie_id = requests.args.get('id')
+    movie_seleted = Movie.query.get(movie_id)
+    db.session.delete(movie_seleted)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/add')
+def add():
+    form = MovieAddingForm()
+    return render_template('add.html', form=form)
+
+
+
 
 
 if __name__ == '__main__':
